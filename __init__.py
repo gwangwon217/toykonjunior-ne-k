71131f0e3503db2ce8df6cf6bd632f0ded2e1acc
gwangwon217 <gwangwon217@gmail.com>
2022-07-12 15:43:37 +0900

    commit

--- __init__.py
+++ __init__.py
@@ -1,14 +1,18 @@
 from mycroft import MycroftSkill, intent_file_handler
-
+import os
 
 class ToykonjuniorNeK(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
-
+        
     @intent_file_handler('k.ne.toykonjunior.intent')
     def handle_k_ne_toykonjunior(self, message):
         self.speak_dialog('k.ne.toykonjunior')
 
+    @intent_file_handler('update.intent')
+    def handle_update(self, message):
+        os.system("git clone https://github.com/gwangwon217/toykonjunior-ne-k.git") 
+        self.speak_dialog('updated success !')
 
 def create_skill():
     return ToykonjuniorNeK()

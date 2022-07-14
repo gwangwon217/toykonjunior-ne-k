5757c006465cd0a45062f43874c6f6998811b06d
gwangwon217 <gwangwon217@gmail.com>
2022-07-14 11:41:47 +0900

    add intent

--- __init__.py
+++ __init__.py
@@ -1,19 +1,41 @@
+import imp
 from mycroft import MycroftSkill, intent_file_handler
-from . import Update
-
+import json
+import sys
+import os
 class ToykonjuniorNeK(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
         
+        ROOT_DIR = os.path.dirname(os.path.abspath(__file__)) # This is your Project Root
+        
+        
+        with open(ROOT_DIR+'/data.json') as json_file:
+            self.json_data = json.load(json_file)
+            print(self.json_data)
+
+
     @intent_file_handler('k.ne.toykonjunior.intent')
     def handle_k_ne_toykonjunior(self, message):
         self.speak_dialog('k.ne.toykonjunior')
 
-    @intent_file_handler('update.intent')
-    def handle_update(self, message):
-        Update.func1()
-        self.speak_dialog('updated success !12312')
-       
+    #@intent_file_handler('update.intent')
+    #def handle_update(self, message):
+    #    self.speak_dialog('updated success !12312')
+
+
+    # 유닛 종료되었을때
+    def endUnit(self):
+        self.speak_dialog('end.dialog')
+
+
+    # 완전히 스킬이 종료되었을때?
+    def stop(self):
+        self.speak_dialog('end.dialog')
+        return super().stop()   
+    def converse(self, message=None):
+        return super().converse(message)
 def create_skill():
     return ToykonjuniorNeK()
 
+ 
